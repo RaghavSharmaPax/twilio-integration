f8f674748b10836d34615e6d0d45cc439d33ca6a
Raghav Sharma <ragsharma@paymentus.com>
2023-07-19 19:13:07 +0530

    update

--- message-handler.js
+++ message-handler.js
@@ -10,6 +10,7 @@ export { MessagingResponse }
 
 client.messaging.v1.services(process.env.TWILIO_MESSAGING_SERVICE_ID)
     .update({
+        preWebhookUrl: process.env.TWILIO_WEBHOOK_URL,
         postWebhookUrl: process.env.TWILIO_WEBHOOK_URL,
         webhookFilters: ['onMessageSent', 'onMessageSend'],
         webhookMethod: 'POST'
