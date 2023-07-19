5fda37dabaf1052ad229c6f609a94c0725b7f542
Raghav Sharma <ragsharma@paymentus.com>
2023-07-19 18:26:52 +0530

    update:

--- message-handler.js
+++ message-handler.js
@@ -8,12 +8,14 @@ const client = Twilio(accountSid, authToken);
 const { MessagingResponse } = Twilio.twiml
 export { MessagingResponse }
 
-client.chat.v2.services(process.env.TWILIO_MESSAGING_SERVICE_ID)
+client.messaging.v1.services(process.env.TWILIO_MESSAGING_SERVICE_ID)
     .update({
         preWebhookUrl: process.env.TWILIO_WEBHOOK_URL,
         postWebhookUrl: process.env.TWILIO_WEBHOOK_URL,
         webhookMethod: 'POST'
     })
     .then(service => console.log(service.friendlyName));
+
+
 
 export default client;
